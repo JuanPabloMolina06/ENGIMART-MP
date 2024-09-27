935d7c9055cce2604ab63d37bd9132013a48b1f3
Juan <juanpablpmolina@gmail.com>
2024-09-27 00:10:29 -0300

    attslaa

--- ENGIMART/src/Componentes-F/FaqF.jsx
+++ ENGIMART/src/Componentes-F/FaqF.jsx
@@ -3,11 +3,11 @@ import Accordion from "./Accordion";
 export default function FaqF() {
     return(
         <>
-            <div className="rounded-lg ">
+            <div className="rounded-lg mb-10">
                 <Accordion
                 title="O que é a ENGIMART?" 
-                answer="A ENGIMART é uma empresa focada em facilitar e dar praticidade ao comprador" />
-                <Accordion 
+                answer="A ENGIMART é um marketplace B2B que junta fornecedores de todos os meios da construção civil para facilitar a compra e venda com construtoras." />
+                <Accordion
                 title="A ENGIMART serve para o meu negócio?"
                 answer="A ENGIMART é uma empresa focada em facilitar e dar praticidade ao comprador" />
                 <Accordion 
